94a47b405c6e921bbd2fe740514773e5c9fde0d6
Abdul Musawir <ap.musawir@gmail.com>
2022-12-09 05:48:14 +0530

    created registration form

--- forms.py
+++ forms.py
@@ -10,15 +10,15 @@ class RegistrationForm(FlaskForm):
     # email = EmailField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField(
-        'Confirm Password', validators=[DataRequired(), EqualTo('Password')])
+        'Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
 
 class LoginForm(FlaskForm):
     # username = StringField('Username', validators=[
     #                        DataRequired(), Length(min=2, max=20)])
-    # email = StringField('Email', validators=[DataRequired(), Email()])
-    email = EmailField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    # email = EmailField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
